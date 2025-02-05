2ad62870d48ed9bff93cb2753ebe94fd4ba7f428
Alberto Alas <oa3973392@gmail.com>
2025-02-04 23:18:56 -0600

    changes saved

--- src/database/DB_connection.js
+++ src/database/DB_connection.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 // SQL Server configuration, use sql server authentication
 const config = {
-    user: 'sa', //usuario de sql server
-    password: '123456', //contraseña de sql server
-    server: 'localhost', // localhost or an IP address
-    database: 'bd_oratorio', // nombre de la base de datos
+    user: process.env.DB_USER, //usuario de sql server
+    password: process.env.DB_PASSWORD, //contraseña de sql server
+    server: process.env.DB_SERVER, // localhost or an IP address
+    database: process.env.DB_NAME, // nombre de la base de datos
     options: {
         encrypt: true, // Use this if you're on Azure
         trustServerCertificate: true // Change to false in production
